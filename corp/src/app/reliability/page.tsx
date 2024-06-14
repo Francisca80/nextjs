666d2f55bc7d68cfef40d56458515b3f6d5ed42b
francisca80 <franciscavanmaurik@gmail.com>
2024-06-14 16:20:46 +0200

    Nextjs_course

---
+++ corp/src/app/reliability/page.tsx
@@ -0,0 +1,6 @@
+import Hero from "@/components/hero";
+import reliabilityImg from "public/reliability.jpg"
+
+export default function ReliabilityPage() {
+    return <Hero imgData={reliabilityImg} imgAlt="welding" title="We serve high performance applications"/>;
+}
